2e3efe9bea3cb4c0bce34476ad44a5f9a979aa52
trushank-21 <trushank38@gmail.com>
2026-02-27 21:37:57 +0530

    neeww0000011

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-
+chmod a+x run.sh
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
--- run.sh
+++ run.sh
@@ -6,3 +6,8 @@ celery -A config worker --loglevel=info --pool=threads &
 
 # Start Gunicorn (Web Server)
 gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
+
+celery -A config worker --loglevel=info --pool=threads --concurrency=1
+
+# 3. Wait for processes
+wait -n
