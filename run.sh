03f3f31f21b79d9b26877b1400233ce4c1c1d4bc
trushank-21 <trushank38@gmail.com>
2026-02-27 21:50:51 +0530

    neeww00000111

--- run.sh
+++ run.sh
@@ -2,12 +2,13 @@
 
 # Start Celery worker in the background
 # celery -A config worker --loglevel=info &
-celery -A config worker --loglevel=info --pool=threads &
+# celery -A config worker --loglevel=info --pool=threads &
+celery -A config worker --loglevel=info --pool=solo --concurrency=1 &
 
 # Start Gunicorn (Web Server)
-gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
+# gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn config.wsgi:application --bind 0.0.0.0:$PORT --workers 2 --threads 2
 
-celery -A config worker --loglevel=info --pool=threads --concurrency=1
+# celery -A config worker --loglevel=info --pool=threads --concurrency=1
 
 # 3. Wait for processes
-wait -n
