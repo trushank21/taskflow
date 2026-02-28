867dc4c2efd319480ccfd9207feb4df954fc7862
trushank-21 <trushank38@gmail.com>
2026-02-28 12:49:40 +0530

    Disable whitenoise manifest storage completely

--- config/settings.py
+++ config/settings.py
@@ -223,11 +223,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
